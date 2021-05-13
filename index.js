c7f5c7e765d93a17b7bdfe0db6f31da959e9be53
rilith <bludoftenchiefs@protonmail.com>
2021-05-12 19:38:41 -0500

    commit tree 49faa640bf8e9f0a30aea9b59c7e45d3872e5d07

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+ 
+function superbowlWin(record) {
+    const results = record.find(x => x.result === "W");
+    console.log(results);
+    if (results) {
+        return results.year;
+    } else {
+        return undefined;
+    }
+}
 
+
+
+    
